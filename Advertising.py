1e3c8191232a306d5250aeb8d73dd275316b8815
liuyudongxiao <52232656+liuyudongxiao@users.noreply.github.com>
2020-11-06 15:22:24 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -13,6 +13,7 @@ from ProAdblock import ProAdblock
 from Blackmatrix7 import Blackmatrix7
 from AdGuardDNS import AdGuardDNS
 from FindDuplicate import FindDuplicate
+from WhiteList import WhiteList
 
 
 
@@ -23,6 +24,15 @@ if __name__ == '__main__':
     ProAdblock.pull()
     AdGuardDNS.pull()
 
+    strWhite = []
+    strWhite = WhiteList.pull()
+    #print(strWhite)
+
+    strWhite2 = []
+    strWhite2 = WhiteList.pull2()
+
+    strWhite3 = []
+    strWhite3 = WhiteList.pull3()
 
     #去重
     readPath='11.txt'
@@ -44,11 +54,20 @@ if __name__ == '__main__':
             str=[]
             str = line
             str = str[0:str.find('/n')]
+            str1 = str[str.find(',')+1: str.rfind(',')]
             if "#" in line:
                 continue
             if line in ['\n','\r\n']:
                 continue
             if line.strip() == "":
+                continue
+            if str1 in strWhite:
+                #print(str)
+                continue
+            if str1 in strWhite2:
+                #print(str)
+                continue
+            if str1 in strWhite3:
                 continue
             # if "HOST,p3.pstatp.com,AdvertisingTest" in str:
             #     continue
@@ -64,42 +83,80 @@ if __name__ == '__main__':
         print('总行数为%s行。' % num)
     f.close()
 
-        #格式
+
+    count = 0
+    #格式
     with open("11.txt","w") as fin:
-        for line in open("cnews.test2.txt"):
+        #for line in open("cnews.test2.txt"):
+        file = open("cnews.test2.txt","r")
+        for num1,line in enumerate(file):
             str=[]
             str = line
             str = str[0:str.find('/n')]
-            if "meituan.net" == str:
-                #print(str)
-                continue
-            if "api.ksapisrv.com" == str:
-                continue
+            if num1 < num-1:
+                if "meituan.net" == str:
+                    #print(str)
+                    count +=1
+                    continue
+                if "api.ksapisrv.com" == str:
+                    count +=1
+                    continue
 
-            if "www.ksapisrv.com" == str:
-                continue
-            if "music.126.net" == str:
-                continue
+                if "www.ksapisrv.com" == str:
+                    count +=1
+                    continue
+                if "music.126.net" == str:
+                    count +=1
+                    continue
+                if str in strWhite:
+                    count +=1
+                    #print(str)
+                    continue
+                if str in strWhite2:
+                    count +=1
+                    #print(str)
+                    continue
+                if str in strWhite3:
+                    count +=1
+                    continue
+                else:
+                    if "meituan.net" == str:
+                        #print(str)
+                        continue
+                    if "api.ksapisrv.com" == str:
+                        continue
+
+                    if "www.ksapisrv.com" == str:
+                        continue
+                    if "music.126.net" == str:
+                        continue
+                    if str in strWhite:
+                        #print(str)
+                        continue
+                    if str in strWhite2:
+                        #print(str)
+                        continue
+                    if str in strWhite3:
+                        continue
 
             str = "HOST-SUFFIX," + str
             str = str + ",REJECT" + "\n"
             fin.write(str)
+        file.close()
         fin.close()
 
 
-
+    print('count: %s' %count)
     file = open("11.txt","r")
     with open("Advertising.list","w") as fin:
         for num1,value in enumerate(file):
-            if(num1 >= num-2):
+            if(num1 >= num-count-1):
                 if ".," in value:
-                    continue
-                if "tos.pstatp.com" in value:
                     continue
 
                 fin.write(value)
             #print("the nume:%s,the value is %s", num, value)
-        file.close()
+    file.close()
     fin.close()
 
 
@@ -108,5 +165,5 @@ if __name__ == '__main__':
     os.remove("11.txt")
     os.remove("7.txt")
     os.remove("77.txt")
-    os.remove("cnews.test2.txt")
+    #os.remove("cnews.test2.txt")
     shutil.rmtree("__pycache__")
