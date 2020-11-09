507bc4f090c5ea3ebeb5d00fce6ef94ac237760a
liuyudongxiao <52232656+liuyudongxiao@users.noreply.github.com>
2020-11-09 11:43:45 +0800

    Update whiteList

--- WhiteList/WhiteList.py
+++ WhiteList/WhiteList.py
@@ -3,10 +3,10 @@ import requests, re, os
 import shutil
 
 
-class WhiteList:
+class whiteList:
     #去重
     def quchong(readPath,writePath):
-        # readPath='WhiteList_1.txt'
+        # readPath='whiteList_1.txt'
         # writePath='whiteList.list'
         lines_seen=set()
         outfiile=open(writePath,'w+')
@@ -34,12 +34,11 @@ class WhiteList:
         url = 'https://raw.githubusercontent.com/Potterli20/filtering/master/filtering.txt'
         html = requests.get(url).text
         #print(html)
-
         with open("whiteList1.txt","w") as f:
             f.write(html)
         f.close()
 
-        fwhite=open("WhiteList_1.txt","a+")
+        fwhite=open("whiteList_1.txt","a+")
         for line in open("whiteList1.txt"):
             if "#" in line:
                 #print(line)
@@ -50,7 +49,6 @@ class WhiteList:
                 continue
             if line.strip() == "":
                 continue
-
             str=[]
             str = line
             if "@@||*" in line:
@@ -61,12 +59,6 @@ class WhiteList:
                 str = str[str.find("@@||")+4:str.rfind("^")] + "\n"
                 fwhite.write(str)
                 continue
-            #str = str[str.find(',')+1: str.rfind(',')] + "\n"
-            
-
-            #str = str[0:str.find(',')] + "\n"
-            #str = "HOST," + str
-            #str = str + ",Advertising" + "\n" 
         fwhite.close()
         return html
     
@@ -74,12 +66,11 @@ class WhiteList:
         url = 'https://raw.githubusercontent.com/liwenjie119/adg-rules/master/white.txt'
         html = requests.get(url).text
         #print(html)
-
         with open("whiteList1.txt","w") as f:
             f.write(html)
         f.close()
 
-        fwhite=open("WhiteList_1.txt","a+")
+        fwhite=open("whiteList_1.txt","a+")
         for line in open("whiteList1.txt"):
             if "#" in line:
                 #print(line)
@@ -91,7 +82,6 @@ class WhiteList:
             if line.strip() == "":
                 continue
 
-
             str=[]
             str = line
             if "@@||*" in line:
@@ -102,31 +92,18 @@ class WhiteList:
                 str = str[str.find("@@||")+4:str.rfind("^")] + "\n"
                 fwhite.write(str)
                 continue
-            #str = str[str.find(',')+1: str.rfind(',')] + "\n"
-            
-
-            #str = str[0:str.find(',')] + "\n"
-            #str = "HOST," + str
-            #str = str + ",Advertising" + "\n" 
         fwhite.close()
         return html
     
-    def pull4():
-        url = 'https://raw.githubusercontent.com/Angelalisa-x/Advertising/master/WhiteList.txt'
-        html = requests.get(url).text
-        #print(html)
-        return html
-
     def pull7():
         url = 'https://raw.githubusercontent.com/etotakeo/AdGuardDNSPassList/master/DNS-Pass-List'
         html = requests.get(url).text
         #print(html)
-
         with open("whiteList1.txt","w") as f:
             f.write(html)
         f.close()
 
-        fwhite=open("WhiteList_1.txt","a+")
+        fwhite=open("whiteList_1.txt","a+")
         for line in open("whiteList1.txt"):
             if "#" in line:
                 #print(line)
@@ -138,7 +115,6 @@ class WhiteList:
             if line.strip() == "":
                 continue
 
-
             str=[]
             str = line
             if "@@||*" in line:
@@ -149,12 +125,6 @@ class WhiteList:
                 str = str[str.find("@@||")+4:str.rfind("^")] + "\n"
                 fwhite.write(str)
                 continue
-            #str = str[str.find(',')+1: str.rfind(',')] + "\n"
-            
-
-            #str = str[0:str.find(',')] + "\n"
-            #str = "HOST," + str
-            #str = str + ",Advertising" + "\n" 
         fwhite.close()
         return html
 
@@ -162,7 +132,7 @@ class WhiteList:
         #url = 'https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall-dns.txt'
         #url = 'https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt'
 
-        fwhite=open("WhiteList_1.txt","w+")
+        fwhite=open("whiteList_1.txt","a+")
         for line in open("kbsmlDns.txt",encoding='UTF-8'):
             str=[]
             str = line
@@ -175,22 +145,61 @@ class WhiteList:
                 fwhite.write(str)
                 continue
         fwhite.close()
+
+    # def pullADgk():
+    #     url = 'https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt'
+    #     html = requests.get(url).text
+    #     with open("whiteList1.txt","w",encoding='UTF-8') as f:
+    #         f.write(html)
+    #     f.close()
+
+    #     fwhite=open("whiteList_1.txt","w")
+    #     for line in open("whiteList1.txt",encoding='UTF-8'):
+    #         str=[]
+    #         str = line
+    #         if "#" in line:
+    #             #print(line)
+    #             continue
+    #         if "!" in line:
+    #             continue
+    #         if "$" in line:
+    #             continue
+    #         if "=" in line:
+    #             continue
+    #         if "?" in line:
+    #             continue
+    #         if "/|" in line:
+    #             continue
+    #         if "/" in line:
+    #             continue
+    #         if "。" in line:
+    #             continue
+    #         if "@@||*" in line:
+    #             str = str[str.find("@@||*")+5:str.rfind("^")] + "\n"
+    #             fwhite.write(str)
+    #             continue
+    #         if "@@||" in line:
+    #             str = str[str.find("@@||")+4:str.rfind("^")] + "\n"
+    #             fwhite.write(str)
+    #             continue
+    #     fwhite.close()    
  
 
 if __name__ == '__main__':
-    WhiteList.pull2()
-    WhiteList.pull3()
-    WhiteList.pull7()
-    WhiteList.pullkbsml()
+    whiteList.pull2()
+    whiteList.pull3()
+    whiteList.pull7()
+    whiteList.pullkbsml()
+    # whiteList.pullADgk()
 
 
-    WhiteList.quchong("WhiteList_1.txt","WhiteList.txt")
-    WhiteList.geshiProcess()
+    whiteList.quchong("whiteList_1.txt","whiteList.txt")
+    whiteList.geshiProcess()
 
 
 
     os.remove("whiteList1.txt")
-    os.remove("WhiteList_1.txt")
+    os.remove("whiteList_1.txt")
     #shutil.rmtree("__pycache__")
 
 
