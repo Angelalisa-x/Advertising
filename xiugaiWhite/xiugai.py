ace01c04fa26b8bcbf5de6b8eb7578c0a82f1c07
liuyudongxiao <52232656+liuyudongxiao@users.noreply.github.com>
2020-11-11 17:25:37 +0800

    Creat xiugaiWhite

---
+++ xiugaiWhite/xiugai.py
@@ -0,0 +1,63 @@
+
+import sys,os
+import shutil
+from pathlib import Path
+
+def copyFileOut():
+    shutil.copy("../WhiteList/swhiteList.txt", os.path.join(os.path.dirname(__file__)))
+    shutil.copy("../CustomRuleAD/CustomRuleAD.py", os.path.join(os.path.dirname(__file__)))
+
+def copyFileIn():
+    shutil.copy("CustomRuleAD_Ex.py", os.path.join(os.path.dirname(__file__),'../CustomRuleAD'))
+
+def DelFile():
+    my_file = Path(os.path.join(os.path.dirname(__file__),'xiugaiWhite\__pycache__'))
+    if my_file.exists():
+        # 指定的文件或目录存在
+        shutil.rmtree(os.path.join(os.path.dirname(__file__),'xiugaiWhite\__pycache__'))
+
+    os.remove("swhiteList.txt")
+    os.remove("CustomRuleAD.py")
+
+    
+
+
+def insertWhite(targetFile,readPath,whitePath):
+    ctrl = 0
+    fin = open(targetFile,"w",encoding='UTF-8')
+    for line in open(readPath,"r",encoding='UTF-8'):
+        str = []
+        str = line
+        str = str[0:str.find("\n")]
+        if "######### yudong white #############" in line:
+            ctrl = 1
+        if ctrl == 1:
+            for whiteLine in open(whitePath,"r",encoding='UTF-8'):
+                strWhite = []
+                strWhite = whiteLine
+                strWhite = str[0:strWhite.find("\n")]
+                if whiteLine == "\n":
+                    continue
+                whiteLine = whiteLine[0:whiteLine.find("\n")]
+
+                strWhite = "        if \"" + whiteLine + "\" in line:" + "\n"
+                fin.write(strWhite)
+                fin.write("            continue" + "\n")
+            ctrl = 0
+        str = str + "\n"
+        fin.write(str)
+    fin.close()
+
+if __name__ == '__main__':
+    copyFileOut()
+    insertWhite("CustomRuleAD_Ex.py","CustomRuleAD.py","swhiteList.txt", )
+
+    copyFileIn()
+    DelFile()
+
+
+    # os.remove("whiteList1.txt")
+    #shutil.rmtree("__pycache__")
+
+
+
