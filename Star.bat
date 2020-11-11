157f569c4f2f17882c92e4af30afe6063a0b9b0d
liuyudongxiao <52232656+liuyudongxiao@users.noreply.github.com>
2020-11-11 17:30:03 +0800

    Super WhiteList

--- Star.bat
+++ Star.bat
@@ -2,7 +2,11 @@ cd WhiteList
 F:/python3.7/python.exe e:/work/Advertising/WhiteList/WhiteList.py
 
 cd ..
+cd xiugaiWhite
+F:/python3.7/python.exe e:/work/Advertising/xiugaiWhite/xiugai.py
+
+cd ..
 cd CustomRuleAD
-F:/python3.7/python.exe e:/work/Advertising/CustomRuleAD/CustomRuleAD.py
+F:/python3.7/python.exe e:/work/Advertising/CustomRuleAD/CustomRuleAD_Ex.py
 
 pause
