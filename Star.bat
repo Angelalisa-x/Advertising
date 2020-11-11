93c933778ae76ede636a5e93bf0d8f42939cc70a
liuyudongxiao <52232656+liuyudongxiao@users.noreply.github.com>
2020-11-11 20:32:10 +0800

    Del

--- Star.bat
+++ Star.bat
@@ -12,4 +12,4 @@ F:/python3.7/python.exe e:/work/Advertising/CustomRuleAD/CustomRuleAD_Ex.py
 del E:\work\Advertising\xiugaiWhite\CustomRuleAD_Ex.py /q /f /s 
 del E:\work\Advertising\CustomRuleAD\CustomRuleAD_Ex.py /q /f /s 
 
-pause
+#pause
