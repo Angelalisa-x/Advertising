808204d25b427c549f6e9d29a6a5317452fa0ce6
Angelalisa-x <52232656+Angelalisa-x@users.noreply.github.com>
2020-11-14 21:26:13 +0800

    Create StarHome.bat

---
+++ StarHome.bat
@@ -0,0 +1,28 @@
+cd WhiteList
+E:/python3.9/python.exe e:/work/Advertising/WhiteList/WhiteList.py
+
+cd ..
+cd xiugaiWhite
+E:/python3.9/python.exe e:/work/Advertising/xiugaiWhite/xiugai.py
+
+cd ..
+cd CustomRuleAD
+E:/python3.9/python.exe e:/work/Advertising/CustomRuleAD/CustomRuleAD_Ex.py
+
+cd ..
+cd Surge
+E:/python3.9/python.exe e:/work/Advertising/Surge/SurgeCustomRuleAD_Ex.py
+
+cd ..
+cd Adguard
+E:/python3.9/python.exe e:/work/Advertising/Adguard/Adguard_Ex.py
+
+del E:\work\Advertising\xiugaiWhite\CustomRuleAD_Ex.py /q /f /s 
+del E:\work\Advertising\xiugaiWhite\SurgeCustomRuleAD_Ex.py /q /f /s
+del E:\work\Advertising\xiugaiWhite\Adguard_Ex.py /q /f /s
+
+del E:\work\Advertising\CustomRuleAD\CustomRuleAD_Ex.py /q /f /s 
+del E:\work\Advertising\Surge\SurgeCustomRuleAD_Ex.py /q /f /s 
+del E:\work\Advertising\Adguard\Adguard_Ex.py /q /f /s
+
+pause
