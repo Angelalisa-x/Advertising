e5161ce272953ab986d9ed7c5d51ae1932ec5ae6
liuyudongxiao <52232656+liuyudongxiao@users.noreply.github.com>
2020-11-23 09:18:42 +0800

    Update

--- Star.bat
+++ Star.bat
@@ -13,10 +13,16 @@ cd ..
 cd Surge
 F:/python3.7/python.exe e:/work/Advertising/Surge/SurgeCustomRuleAD_Ex.py
 
+cd ..
+cd Adguard
+F:/python3.7/python.exe e:/work/Advertising/Adguard/Adguard_Ex.py
+
 del E:\work\Advertising\xiugaiWhite\CustomRuleAD_Ex.py /q /f /s 
-del E:\work\Advertising\xiugaiWhite\SurgeCustomRuleAD_Ex.py /q /f /s 
+del E:\work\Advertising\xiugaiWhite\SurgeCustomRuleAD_Ex.py /q /f /s
+del E:\work\Advertising\xiugaiWhite\Adguard_Ex.py /q /f /s
 
 del E:\work\Advertising\CustomRuleAD\CustomRuleAD_Ex.py /q /f /s 
 del E:\work\Advertising\Surge\SurgeCustomRuleAD_Ex.py /q /f /s 
+del E:\work\Advertising\Adguard\Adguard_Ex.py /q /f /s
 
 pause
