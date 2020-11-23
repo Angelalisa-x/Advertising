3d3ad9502f51695847dad6d35b9f9bb43852c959
liuyudongxiao <52232656+liuyudongxiao@users.noreply.github.com>
2020-11-23 11:02:37 +0800

    Update

--- WhiteList/WhiteList.py
+++ WhiteList/WhiteList.py
@@ -288,6 +288,49 @@ def pullWhite():
             continue
     fwhite.close()
             ############# lhzgl6587 End    ################
+
+            ############# DivineEngine Star   ################
+    url = 'https://raw.githubusercontent.com/DivineEngine/AdGuardFilter/master/filter.txt'
+    html = requests.get(url).text
+    with open("whiteList1.txt","w") as f:
+        f.write(html)
+    f.close()
+
+    fwhite=open("whiteList_1.txt","a+")
+    for line in open("whiteList1.txt"):
+        if "#" in line:
+            #print(line)
+            continue
+        if "!" in line:
+            continue
+        if line in ['\n','\r\n']:
+            continue
+        if line.strip() == "":
+            continue
+        str=[]
+        str = line
+        if "@@||*" in line:
+            if "^" in line:
+                str = str[str.find("@@||*")+5:str.rfind("^")] + "\n"
+                fwhite.write(str)
+                continue
+        if "@@||" in line:
+            if "^" in line:
+                str = str[str.find("@@||")+4:str.rfind("^")] + "\n"
+                fwhite.write(str)
+                continue
+        if "@@||*." in line:
+            if "^" in line:
+                str = str[str.find("@@||*.")+6:str.rfind("^")] + "\n"
+                fwhite.write(str)
+                continue
+        if "@@||." in line:
+            if "^" in line:
+                str = str[str.find("@@||.")+5:str.rfind("^")] + "\n"
+                fwhite.write(str)
+                continue
+    fwhite.close()
+            ############# DivineEngine End    ################
  
 if __name__ == '__main__':
     pullWhite()
