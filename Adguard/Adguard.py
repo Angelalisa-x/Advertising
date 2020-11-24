fd91e1a9920b4b949839e9b7416d5cec7c81ffba
liuyudongxiao <52232656+liuyudongxiao@users.noreply.github.com>
2020-11-24 10:49:45 +0800

    Update Adguard

--- Adguard/Adguard.py
+++ Adguard/Adguard.py
@@ -41,27 +41,8 @@ def Deduplication(writePath,readPath):
     outfiile.close()
     f.close()
 
-def pull():
-    ################# anti-ad #################
-    url = 'https://anti-ad.net/easylist.txt'
-    html = requests.get(url).text
-
-    fin = open("temporary.txt","w",encoding='UTF-8')
-    fin.write(html)
-    fin.close()
-
-    with open("Adguard.txt","w",encoding='UTF-8') as fin7:
-        for line in open("temporary.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
-                continue
-            fin7.write(str)
-        fin7.close()
-
-    ############## Adgk ##############
-    url = 'https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt'
+def pullWangzhanInfo(urlInfo):
+    url = urlInfo
     html = requests.get(url).text
 
     fin = open("temporary.txt","w",encoding='UTF-8')
@@ -69,253 +50,71 @@ def pull():
     fin.close()
 
     with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
+        fin7.write("\n")
         for line in open("temporary.txt",encoding='UTF-8'):
             str = []
             str = line
             str = str[0:str.find("\n")] + "\n"
             if "!" in line:
                 continue
-            fin7.write(str)
-        fin7.close()
-
-    ####################    kbsml  ############
-    #url = 'https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt '
-
-    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
-        for line in open("kbsm.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
+            if "#" in line:
                 continue
-            fin7.write(str)
-        fin7.close()
-
-    ##################  lhzgl6587   ###############
-    url = 'https://gitee.com/lhzgl6587/hosts/raw/master/myruler '
-    html = requests.get(url).text
-
-    fin = open("temporary.txt","w",encoding='UTF-8')
-    fin.write(html)
-    fin.close()
- 
-    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
-        for line in open("temporary.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
-                continue
-            fin7.write(str)
-        fin7.close()
-
-    ##################  jdlingyu   ###############
-    url = 'https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts'
-    html = requests.get(url).text
-
-    fin = open("temporary.txt","w",encoding='UTF-8')
-    fin.write(html)
-    fin.close()
- 
-    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
-        for line in open("temporary.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
-                continue
-            if "127.0.0.1 " in line:
-                str = str[str.find("127.0.0.1 ")+10:str.find("\n")]
-                str = "||" + str + "^" + "\n"
-                fin7.write(str)
-        fin7.close()
-
-    ##################  neodevpro  ###############
-    url = 'https://raw.githubusercontent.com/neodevpro/neodevhost/master/host'
-    html = requests.get(url).text
-
-    fin = open("temporary.txt","w",encoding='UTF-8')
-    fin.write(html)
-    fin.close()
- 
-    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
-        for line in open("temporary.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
-                continue
-            if "0.0.0.0  " in line:
-                str = str[str.find("0.0.0.0  ")+9:str.find("\n")]
-                str = "||" + str + "^" + "\n"
-                fin7.write(str)
-        fin7.close()
-
-    ##################  AdAway  ###############
-    url = 'https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt'
-    html = requests.get(url).text
-
-    fin = open("temporary.txt","w",encoding='UTF-8')
-    fin.write(html)
-    fin.close()
- 
-    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
-        for line in open("temporary.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
-                continue
-            if line == '\n':
+            if "@@||" in line:
                 continue
             if "localhost" in line:
                 continue
-            if "127.0.0.1 " in line:
-                str = str[str.find("127.0.0.1 ")+10:str.find("\n")]
-                str = "||" + str + "^" + "\n"
-                fin7.write(str)
-        fin7.close()
-
-########################################################################################################################
-def PullDirectly():
-
-    ################## anti-ad ##########################
-    url = 'https://anti-ad.net/easylist.txt'
-    html = requests.get(url).text
-
-    fin = open("temporary.txt","w",encoding='UTF-8')
-    fin.write(html)
-    fin.close()
- 
-    with open("Adguard.txt","w",encoding='UTF-8') as fin7:
-        for line in open("temporary.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
-                continue
-            if line == '\n':
-                continue
-            if "@@||" in line:
-                continue
             fin7.write(str)
         fin7.close()
 
-    #################### kbsml DNS ############
+def pullKbsml():
+    #url = 'https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt '
+
+    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
+        fin7.write("\n")
+        for line in open("kbsm.txt",encoding='UTF-8'):
+            if "!" in line:
+                continue
+            if "#" in line:
+                continue
+            if "@@||" in line:
+                continue
+            fin7.write(line)
+        fin7.close()
+
+def pullKbsmlDns():
     #url = 'https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall-dns.txt'
 
-    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
+    with open("Adguard.txt","w",encoding='UTF-8') as fin7:
         for line in open("../WhiteList/kbsmlDns.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
             if "!" in line:
-                continue
-            if line == '\n':
-                continue
-            if "@@||" in line:
-                continue
-            fin7.write(str)
-        fin7.close()
-
-    ##################### AdAway ###################
-    url = 'https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt'
-    html = requests.get(url).text
-
-    fin = open("temporary.txt","w",encoding='UTF-8')
-    fin.write(html)
-    fin.close()
- 
-    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
-        for line in open("temporary.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
-                continue
-            if line == '\n':
-                continue
-            if "@@||" in line:
                 continue
             if "#" in line:
                 continue
-            fin7.write(str)
-        fin7.close()
-
-    ################## lhzgl6587 ##################
-    url = 'https://gitee.com/lhzgl6587/hosts/raw/master/myruler'
-    html = requests.get(url).text
-
-    fin = open("temporary.txt","w",encoding='UTF-8')
-    fin.write(html)
-    fin.close()
- 
-    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
-        for line in open("temporary.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
-                continue
-            if line == '\n':
-                continue
             if "@@||" in line:
                 continue
-            if "#" in line:
-                continue
-            fin7.write(str)
-        fin7.close()
-
-    ################ jdlingyu ##################
-    url = 'https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts'
-    html = requests.get(url).text
-
-    fin = open("temporary.txt","w",encoding='UTF-8')
-    fin.write(html)
-    fin.close()
- 
-    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
-        for line in open("temporary.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
-                continue
-            if line == '\n':
-                continue
-            if "@@||" in line:
-                continue
-            if "#" in line:
-                continue
-            fin7.write(str)
-        fin7.close()
-
-    #################### anye1998 #######################
-    url = 'https://gitee.com/anye1998/Adguard-List-of-personal-rules/raw/master/List-of-personal-rules.txt'
-    html = requests.get(url).text
-
-    fin = open("temporary.txt","w",encoding='UTF-8')
-    fin.write(html)
-    fin.close()
- 
-    with open("Adguard.txt","a+",encoding='UTF-8') as fin7:
-        for line in open("temporary.txt",encoding='UTF-8'):
-            str = []
-            str = line
-            str = str[0:str.find("\n")] + "\n"
-            if "!" in line:
-                continue
-            if line == '\n':
-                continue
-            if "#" in line:
-                continue
-            fin7.write(str)
+            fin7.write(line)
         fin7.close()
 
 
 if __name__ == "__main__":
-    pull()
-    #PullDirectly()
+
+    pullKbsmlDns()
+    pullKbsml()
+    pullWangzhanInfo('https://anti-ad.net/easylist.txt')
+    pullWangzhanInfo('https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt')
+    pullWangzhanInfo('https://paulgb.github.io/BarbBlock/blacklists/hosts-file.txt')
+    pullWangzhanInfo('https://gitee.com/lhzgl6587/hosts/raw/master/myruler')
+    pullWangzhanInfo('https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts')
+    pullWangzhanInfo('https://gitee.com/anye1998/Adguard-List-of-personal-rules/raw/master/List-of-personal-rules.txt')
+    pullWangzhanInfo('https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt')
+    pullWangzhanInfo('https://raw.githubusercontent.com/DivineEngine/AdGuardFilter/master/filter.txt')
+    pullWangzhanInfo('https://raw.githubusercontent.com/neodevpro/neodevhost/master/host')
+    pullWangzhanInfo('https://raw.githubusercontent.com/Potterli20/filtering/master/purification')
+    pullWangzhanInfo('https://adaway.org/hosts.txt')
+    pullWangzhanInfo('https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts')
+    pullWangzhanInfo('https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts')
+
+    
 
     Deduplication("Adguard_1.txt","Adguard.txt")
     addWhite("AdguardEx.txt","Adguard_1.txt")
