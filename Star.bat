f64cf1d2bb13709b1b1d8ab5831c84f4c2962c18
liuyudongxiao <52232656+liuyudongxiao@users.noreply.github.com>
2020-11-25 18:36:15 +0800

    echo path

--- Star.bat
+++ Star.bat
@@ -1,28 +1,32 @@
+@echo off 
+
 cd WhiteList
-F:/python3.7/python.exe e:/work/Advertising/WhiteList/WhiteList.py
+py %cd%\WhiteList.py
 
 cd ..
 cd xiugaiWhite
-F:/python3.7/python.exe e:/work/Advertising/xiugaiWhite/xiugai.py
+py %cd%\xiugai.py
 
 cd ..
 cd CustomRuleAD
-F:/python3.7/python.exe e:/work/Advertising/CustomRuleAD/CustomRuleAD_Ex.py
+py %cd%\CustomRuleAD_Ex.py
 
 cd ..
 cd Surge
-F:/python3.7/python.exe e:/work/Advertising/Surge/SurgeCustomRuleAD_Ex.py
+py %cd%\SurgeCustomRuleAD_Ex.py
 
 cd ..
 cd Adguard
-F:/python3.7/python.exe e:/work/Advertising/Adguard/Adguard_Ex.py
+py %cd%\Adguard_Ex.py
 
-del E:\work\Advertising\xiugaiWhite\CustomRuleAD_Ex.py /q /f /s 
-del E:\work\Advertising\xiugaiWhite\SurgeCustomRuleAD_Ex.py /q /f /s
-del E:\work\Advertising\xiugaiWhite\Adguard_Ex.py /q /f /s
+cd ..
 
-del E:\work\Advertising\CustomRuleAD\CustomRuleAD_Ex.py /q /f /s 
-del E:\work\Advertising\Surge\SurgeCustomRuleAD_Ex.py /q /f /s 
-del E:\work\Advertising\Adguard\Adguard_Ex.py /q /f /s
+del %cd%\xiugaiWhite\CustomRuleAD_Ex.py \q \f \s 
+del %cd%\xiugaiWhite\SurgeCustomRuleAD_Ex.py \q \f \s
+del %cd%\xiugaiWhite\Adguard_Ex.py \q \f \s
 
-#pause
+del %cd%\CustomRuleAD\CustomRuleAD_Ex.py \q \f \s 
+del %cd%\Surge\SurgeCustomRuleAD_Ex.py \q \f \s 
+del %cd%\Adguard\Adguard_Ex.py \q \f \s
+
+pause
